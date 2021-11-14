5fb145e517c785e4ceb5a19166af53c97489fcd1
Manuel <manuel_8ale@hotmail.com>
2021-11-13 20:46:31 -0400

    add script

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".anim_section");
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -300,
+  ease: "none", // <-- IMPORTANT!
+  scrollTrigger: {
+    trigger: "#parallax-container",
+    pin: true,
+    scrub: 0.1,
+    //snap: directionalSnap(1 / (sections.length - 1)),
+    end: "+=3000",
+  },
+});
+
+/*gsap.to(".section_two .houses1", {
+  xPercent: 20,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section_two .houses1",
+    containerAnimation: scrollTween,
+    start: "left left",
+    end: "right 100%",
+    toggleActions: "play none none reset",
+    id: "1",
+    scrub: true,
+  },
+});
+
+gsap.to(".section_two .houses2", {
+  xPercent: 10,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".section_two .houses2",
+    containerAnimation: scrollTween,
+    start: "left left",
+    end: "right 100%",
+    toggleActions: "play none none reset",
+    id: "1",
+    scrub: true,
+  },
+});*/
